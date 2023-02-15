66f00dfd57dfd43316f5419bd5dfccc375e7d20d
CodingCarlos <hola@codingcarlos.com>
2023-02-15 10:28:39 +0100

    demo de la api

---
+++ demo-api-cloud-functions.js
@@ -0,0 +1,48 @@
+const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+const express = require('express');
+
+admin.initializeApp();
+
+const db = admin.firestore();
+const app = express();
+
+app.get('/:collection', async (request, response)=>{
+  const ref = db.collection(request.params.collection);
+  const snapshot = await ref.get();
+
+  const result = []
+  snapshot.forEach(doc => {
+    result.push({
+      ...doc.data(),
+      id: doc.id, 
+    });
+  });
+
+  response.send(result);
+});
+
+app.get('/:collection/:id', async (request, response)=>{
+  const ref = db.collection(request.params.collection).doc(request.params.id);
+  const snapshot = await ref.get();
+
+  response.send({
+    ...snapshot.data(),
+    id: snapshot.id,
+  });
+});
+
+app.post('/:collection', async (request, response)=>{
+  if (!request.body) {
+    return response.send('error');
+  }
+
+  const res = await db.collection(request.params.collection).add(request.body);
+
+  response.send({
+    ...request.body,
+    id: res.id,
+  });
+});
+
+exports.app = functions.https.onRequest(app);
